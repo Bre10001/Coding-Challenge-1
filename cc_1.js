073f63a2baf09e97b5012e1b072095dbf43c2cd2
Breanna Burris <breanna1burris@gmail.com>
2024-08-31 01:29:15 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,16 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+//Task 2 Compound Data Types
+
+let products = ["Mini Purse", "Tote Bag", "Luggage"];
+const productDetails = {
+            name: "blu",
+            price: 2400.00
+            inStock: true,
+}
+
+console.log(products, productDetails);
+
+
